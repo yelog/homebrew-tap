7ab89c88b3b45ca12ea4e4396c0dd0b5c8c1c8bf
Logan Ye <yelogeek@gmail.com>
2026-06-04 10:57:30 +0800

    Add vibebar cask

---
+++ Casks/vibebar.rb
@@ -0,0 +1,34 @@
+cask "vibebar" do
+  version "1.3.7"
+  sha256 "32753820a67bcfc417a94687f90325b8d0c16c6a462605cd4b837a11a792375f"
+
+  url "https://github.com/yelog/VibeBar/releases/download/v#{version}/VibeBar-#{version}-universal.dmg"
+  name "VibeBar"
+  desc "Menu bar app for monitoring AI coding sessions"
+  homepage "https://github.com/yelog/VibeBar"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+(?:-(?:beta|alpha|rc)\.?\d*)?)$/i)
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+  app "VibeBar.app"
+
+  postflight do
+    FileUtils.mkdir_p("#{Dir.home}/Library/Application Support/VibeBar/sessions")
+    FileUtils.mkdir_p("#{Dir.home}/Library/Application Support/VibeBar/runtime")
+  end
+
+  uninstall quit: "com.vibebar.app"
+
+  zap trash: [
+    "~/Library/Application Support/VibeBar",
+    "~/Library/Preferences/com.vibebar.app.plist",
+    "~/Library/Caches/com.vibebar.app",
+    "~/Library/Logs/VibeBar",
+  ]
+end
